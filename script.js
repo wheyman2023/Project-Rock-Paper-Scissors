3d36bd632557f67a095a985e8f81e42ddefb0eac
wheyman2023 <46344524+wheyman2023@users.noreply.github.com>
2023-09-17 01:47:36 +0300

    Added conditional for function playround()

--- script.js
+++ script.js
@@ -2,19 +2,63 @@
 // learn how to arrays
 // learn to randomize array list (strings)
 // learn the game concept
-// comparison between user selection and computer selection
 // Combine the playerSelection and computerSelection to 1 func
+// Conditional between user selection and computer selection
+// new function for called game() for 5 rounds
 
 function playRound(playerSelection, computerSelection) {
   // your code here!
+  if (playerSelection == computerSelection) {
+    console.log("User choose:", playerSelection);
+    console.log("Computer choose:", computerSelection);
+    return "Result: It's a tie";
+  } else if (playerSelection) {
+    if (computerSelection == "Paper") {
+      // computer
+      console.log("User choose:", playerSelection);
+      console.log("Computer choose:", computerSelection);
+      return "Result: Computer won!, Paper beats Rock";
+    } else computerSelection == "Scissors"; // computer
+    console.log("User choose:", playerSelection);
+    console.log("Computer choose:", computerSelection);
+    return "Result: User won!, Rock beats Scissors ";
+  }
 }
+// for prompt
+// } else if (playerSelection == "Paper") {
+//   if (computerSelection == "Rocks") {
+//     // computer
+//     console.log("User choose:", playerSelection);
+//     console.log("Computer choose:", computerSelection);
+//     return "Result: User won!, Paper beats Rock";
+//   } else computerSelection == "Scissors";
+//   console.log("User choose:", playerSelection);
+//   console.log("Computer choose:", computerSelection);
+//   return "Results: Computer won!, Scissors beats Paper";
+// } else if (playerSelection == "Paper") {
+//   // user choice
+//   if (computerSelection == "Rock") {
+//     // computer choice
+//     console.log("User choose:", playerSelection);
+//     console.log("Computer choose:", computerSelection);
+//     return "Result: User won!, Paper beats Rock";
+//   } else computerSelection == "Scissors"; //computer choice
+//   console.log("User choose:", playerSelection);
+//   console.log("Computer choose:", computerSelection);
+//   return "Result: Computer won!, Scissors beats Paper";
+// }
 
 // function for getting computer choices
 function getComputerChoice() {
   const choice = ["Rock", "Paper", "Scissors"]; // array for computer's choice
-  return console.log(choice[Math.floor(Math.random() * choice.length)]);
+  return choice[Math.floor(Math.random() * choice.length)]; // return the random choices
 }
+// function getUserChoice() {
+//   const userChoice = "Rock";
+//   return console.log(userChoice);
+// }
 
-const playerSelection = prompt("Rock, Paper, Scissors:");
+// const playerSelection = prompt("Choose: Rock, Paper, Scissors"); // for prompt user
+const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
